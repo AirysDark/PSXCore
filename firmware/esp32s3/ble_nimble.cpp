765d1aba3702a941f3269b9f729541095afd29bf
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 02:59:49 +1000

    Add ESP32-S3 NimBLE BLE HID implementation layer

---
+++ firmware/esp32s3/ble_nimble.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+// PSXCore BLE HID layer
+// ESP32-S3 NimBLE integration point.
+
+void ble_init() {
+  // TODO: initialise NimBLE HID gamepad service.
+}
+
+void ble_send_report() {
+  // TODO: send controller_state as HID report.
+}
