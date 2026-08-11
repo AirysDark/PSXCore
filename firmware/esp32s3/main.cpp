dafe4ac932d23224c04fddd220f68737a46f6045
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 03:00:14 +1000

    Integrate PSX analog enable into ESP32-S3 firmware flow

--- firmware/esp32s3/main.cpp
+++ firmware/esp32s3/main.cpp
@@ -6,9 +6,11 @@ void psxBegin();
 void psxReadController();
 void bleGamepadBegin();
 void bleGamepadUpdate();
+bool psx_enable_analog_mode();
 
 void setup() {
   psxBegin();
+  psx_enable_analog_mode();
   bleGamepadBegin();
 }
 
