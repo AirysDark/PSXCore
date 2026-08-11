966a6da171268729d1c1a70f7aab5b92bc9d2f90
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 04:05:29 +1000

    Expand DS4 auth standalone I2C transport skeleton

--- ds4_auth/auth_i2c.cpp
+++ ds4_auth/auth_i2c.cpp
@@ -1,25 +1,29 @@
 #include "auth_i2c.h"
 
-// Standalone module only.
-// No PSXCore integration yet.
-// This will later implement the DS4 authentication I2C transaction.
+// Standalone DS4 authentication I2C transport.
+// No PSXCore integration.
 
-bool DS4AuthI2C::begin()
+#define DS4_AUTH_WRITE_ADDR 0x60
+#define DS4_AUTH_READ_ADDR  0x61
+
+bool DS4AuthI2C::begin(int sda, int scl)
 {
+    // ESP32 Wire initialization will be added here.
+    (void)sda;
+    (void)scl;
     return true;
 }
 
 bool DS4AuthI2C::sendChallenge(uint8_t tid, const uint8_t challenge[32], uint8_t response[42])
 {
+    // DS4 auth transaction:
+    // TX: [command][TID][32 byte challenge]
+    // RX: [2 byte header][SHA1 result 1][SHA1 result 2]
+
     (void)tid;
     (void)challenge;
     (void)response;
 
-    // TODO:
-    // Write:
-    // 0x60 + command + TID + 32 byte challenge
-    // Read:
-    // 42 byte authentication response
-
+    // Hardware transport pending.
     return false;
 }
