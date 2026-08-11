8182b5a71f26a04eadfec3983151634778cca6e9
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 02:39:21 +1000

    Add ESP32-S3 pin configuration

---
+++ firmware/esp32s3/pins.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// PSX/PS2 controller interface pins
+#define PSX_DATA      4
+#define PSX_COMMAND   5
+#define PSX_ATTENTION 6
+#define PSX_CLOCK     7
+#define PSX_ACK       8
+
+// Future ESP32-S3 BLE HID configuration goes here
