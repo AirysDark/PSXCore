e523111ab73ebaa02b67aef800278ddfcd96fb43
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 03:49:25 +1000

    Add ESP32-S3 SD update pin definitions

--- include/pins.h
+++ include/pins.h
@@ -7,4 +7,11 @@
 #define PSX_CLOCK     7
 #define PSX_ACK       8
 
+// SD card firmware updater pins
+// Change these if your ESP32-S3 Tiny board uses different wiring
+#define SD_CS         10
+#define SD_MOSI       11
+#define SD_MISO       13
+#define SD_SCK        12
+
 // Future ESP32-S3 BLE HID configuration goes here
