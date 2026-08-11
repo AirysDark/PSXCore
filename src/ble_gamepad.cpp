677c51be36ddeea9a7c79bd111e684a8287cbf25
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 03:27:43 +1000

    Fix BLE gamepad module header integration

--- src/ble_gamepad.cpp
+++ src/ble_gamepad.cpp
@@ -1,13 +1,13 @@
 // PSXCore Bluetooth HID layer
 // ESP32-S3 converts PSX controller input into a wireless gamepad.
 
-#include "pins.h"
+#include "ble_gamepad.h"
+#include "controller_state.h"
 
 void bleGamepadBegin() {
-  // TODO: initialize BLE HID gamepad
-  // Buttons and analog axes will map here.
+  // BLE HID initialization will be implemented here.
 }
 
 void bleGamepadUpdate() {
-  // TODO: send controller reports
+  // Controller state report transmission will be implemented here.
 }
