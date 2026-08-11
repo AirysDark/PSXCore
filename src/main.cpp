d00b4d9ca7d474e333e4c57cd28158173eaebbc0
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-12 03:42:36 +1000

    Add SD boot update check

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 #include "psx_reader.h"
 #include "ble_gamepad.h"
 #include "psx_config.h"
+#include "sd_update.h"
 
 void setup() {
+  Serial.begin(115200);
+
+  // Check SD card before normal startup.
+  // If no update exists, continue boot normally.
+  sdUpdateCheck();
+
   psxBegin();
   psx_enable_analog_mode();
   bleGamepadBegin();
