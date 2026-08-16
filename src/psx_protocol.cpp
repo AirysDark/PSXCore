5f2c857b74850635dd08d3c3b96faad5fd0973e8
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-17 02:34:27 +1000

    Use runtime PSX pin mapping in protocol engine

--- src/psx_protocol.cpp
+++ src/psx_protocol.cpp
@@ -5,16 +5,16 @@
 static bool psxLastAck = false;
 
 void psxProtocolInit() {
-  pinMode(PSX_DATA, INPUT_PULLUP);
-  pinMode(PSX_COMMAND, OUTPUT);
-  pinMode(PSX_ATTENTION, OUTPUT);
-  pinMode(PSX_CLOCK, OUTPUT);
-  pinMode(PSX_ACK, INPUT_PULLUP);
+  pinMode(psxPins.data, INPUT_PULLUP);
+  pinMode(psxPins.command, OUTPUT);
+  pinMode(psxPins.attention, OUTPUT);
+  pinMode(psxPins.clock, OUTPUT);
+  pinMode(psxPins.ack, INPUT_PULLUP);
 
   // PSX bus idle state.
-  digitalWrite(PSX_COMMAND, HIGH);
-  digitalWrite(PSX_ATTENTION, HIGH);
-  digitalWrite(PSX_CLOCK, HIGH);
+  digitalWrite(psxPins.command, HIGH);
+  digitalWrite(psxPins.attention, HIGH);
+  digitalWrite(psxPins.clock, HIGH);
 }
 
 uint8_t psxTransferByte(uint8_t value) {
@@ -23,17 +23,17 @@ uint8_t psxTransferByte(uint8_t value) {
   // PSX serial timing is LSB first. The controller changes DATA while the
   // clock is low; sample it before returning the clock high.
   for (int bit = 0; bit < 8; bit++) {
-    digitalWrite(PSX_COMMAND, (value >> bit) & 1);
+    digitalWrite(psxPins.command, (value >> bit) & 1);
     delayMicroseconds(1);
 
-    digitalWrite(PSX_CLOCK, LOW);
+    digitalWrite(psxPins.clock, LOW);
     delayMicroseconds(2);
 
-    if (digitalRead(PSX_DATA)) {
+    if (digitalRead(psxPins.data)) {
       result |= (1 << bit);
     }
 
-    digitalWrite(PSX_CLOCK, HIGH);
+    digitalWrite(psxPins.clock, HIGH);
     delayMicroseconds(2);
   }
 
@@ -42,7 +42,7 @@ uint8_t psxTransferByte(uint8_t value) {
   psxLastAck = false;
   const uint32_t start = micros();
   while ((micros() - start) < 250) {
-    if (digitalRead(PSX_ACK) == LOW) {
+    if (digitalRead(psxPins.ack) == LOW) {
       psxLastAck = true;
       break;
     }
@@ -51,7 +51,7 @@ uint8_t psxTransferByte(uint8_t value) {
   // Allow the controller to release ACK before the next byte.
   if (psxLastAck) {
     const uint32_t ackStart = micros();
-    while ((micros() - ackStart) < 250 && digitalRead(PSX_ACK) == LOW) {
+    while ((micros() - ackStart) < 250 && digitalRead(psxPins.ack) == LOW) {
       delayMicroseconds(1);
     }
   }
