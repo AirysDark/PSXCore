d1af33cffa7dc0767c592abddc208cc4c75244ea
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-16 14:44:00 +1000

    Add PSX pin aliases for transaction layer

--- include/pins.h
+++ include/pins.h
@@ -7,6 +7,13 @@
 #define PSX_CLOCK     7
 #define PSX_ACK       8
 
+// Compatibility aliases used by PSX transaction engine
+#define PIN_DATA       PSX_DATA
+#define PIN_COMMAND    PSX_COMMAND
+#define PIN_ATTENTION  PSX_ATTENTION
+#define PIN_CLOCK      PSX_CLOCK
+#define PIN_ACK        PSX_ACK
+
 // SD card firmware updater pins
 // Change these if your ESP32-S3 Tiny board uses different wiring
 #define SD_CS         10
