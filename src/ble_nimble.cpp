9adf67e089ad3670b5a96c7d63e3aa40ce983595
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-16 15:26:32 +1000

    Connect BLE state to runtime diagnostics

--- src/ble_nimble.cpp
+++ src/ble_nimble.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <BleGamepad.h>
 #include "controller_state.h"
+#include "debug_status.h"
 
 BleGamepad bleGamepad("PSXCore ESP32-S3", "AirysDark", 100);
 
@@ -9,7 +10,10 @@ void ble_init() {
 }
 
 void ble_send_report() {
-  if (!bleGamepad.isConnected()) return;
+  bool connected = bleGamepad.isConnected();
+  debugStatusBLEState(connected);
+
+  if (!connected) return;
 
   bleGamepad.setLeftThumb(controllerState.lx, controllerState.ly);
   bleGamepad.setRightThumb(controllerState.rx, controllerState.ry);
@@ -19,6 +23,7 @@ void ble_send_report() {
 
 void bleGamepadBegin() {
   ble_init();
+  debugStatusBLEState(false);
 }
 
 void bleGamepadUpdate() {
