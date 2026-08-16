78b009b7e468a12c6cc659011faa2108e8ae21af
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-17 02:34:06 +1000

    Add persistent PSX pin mapping storage

---
+++ src/pins.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <Preferences.h>
+#include "pins.h"
+
+PsxPins psxPins = {
+  PSX_DEFAULT_DATA,
+  PSX_DEFAULT_COMMAND,
+  PSX_DEFAULT_ATTENTION,
+  PSX_DEFAULT_CLOCK,
+  PSX_DEFAULT_ACK
+};
+
+static Preferences preferences;
+static bool preferencesOpen = false;
+
+void psxPinsBegin() {
+  if (preferences.begin("psxcore", true)) {
+    preferencesOpen = true;
+
+    if (preferences.getBool("valid", false)) {
+      psxPins.data = preferences.getInt("data", PSX_DEFAULT_DATA);
+      psxPins.command = preferences.getInt("cmd", PSX_DEFAULT_COMMAND);
+      psxPins.attention = preferences.getInt("att", PSX_DEFAULT_ATTENTION);
+      psxPins.clock = preferences.getInt("clk", PSX_DEFAULT_CLOCK);
+      psxPins.ack = preferences.getInt("ack", PSX_DEFAULT_ACK);
+    }
+
+    preferences.end();
+    preferencesOpen = false;
+  }
+}
+
+void psxSetPins(const PsxPins& pins, bool persist) {
+  psxPins = pins;
+
+  if (!persist) {
+    return;
+  }
+
+  if (preferences.begin("psxcore", false)) {
+    preferencesOpen = true;
+    preferences.putBool("valid", true);
+    preferences.putInt("data", psxPins.data);
+    preferences.putInt("cmd", psxPins.command);
+    preferences.putInt("att", psxPins.attention);
+    preferences.putInt("clk", psxPins.clock);
+    preferences.putInt("ack", psxPins.ack);
+    preferences.end();
+    preferencesOpen = false;
+  }
+}
+
+void psxResetPins() {
+  psxPins = {
+    PSX_DEFAULT_DATA,
+    PSX_DEFAULT_COMMAND,
+    PSX_DEFAULT_ATTENTION,
+    PSX_DEFAULT_CLOCK,
+    PSX_DEFAULT_ACK
+  };
+
+  if (preferences.begin("psxcore", false)) {
+    preferences.clear();
+    preferences.end();
+  }
+}
