979dfe4345204e2219e315c64663576d6cc56f00
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-03 18:00:48 +1000

    Remove obsolete Android companion references

--- include/ble_gamepad.h
+++ include/ble_gamepad.h
@@ -2,7 +2,5 @@
 
 #include <Arduino.h>
 
-// Bluetooth HID gamepad only. The former Android companion/configuration
-// GATT service has been removed completely.
 void bleGamepadBegin();
 void bleGamepadUpdate();
